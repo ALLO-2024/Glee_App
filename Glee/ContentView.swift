ea5cd2949b8269fea0eb882685b136e7a1188e25
ddrongy <ddrongy@gmail.com>
2024-01-15 17:25:28 +0900

    Initial Commit

---
+++ Glee/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Glee
+//
+//  Created by 최재혁 on 2024/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glee/GleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GleeApp.swift
+//  Glee
+//
+//  Created by 최재혁 on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct GleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
