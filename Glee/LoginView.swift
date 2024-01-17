d58cf7362130feb234765ef3a684c84566ebab8a
gomhyeok <gomhyeok@gmail.com>
2024-01-17 18:54:02 +0900

    feat : Login 화면 추가

---
+++ Glee/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Glee
+//
+//  Created by 최재혁 on 2024/01/17.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
