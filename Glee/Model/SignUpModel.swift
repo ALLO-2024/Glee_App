25735e2167e83f960415effd95d2cdd030bb802e
gomhyeok <gomhyeok@gmail.com>
2024-02-03 00:35:02 +0900

    Feat: SignupModel의 데이터 구조 변경 - 백엔드의 데이터 구조 변경에 따른 구조 변경 - Request시 필요한 데이터 구조 추가

--- Glee/Model/SignUpModel.swift
+++ Glee/Model/SignUpModel.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 struct SignupResponse : Codable {
-    let status: Int
-    let success: Bool?
+    let isSucces : Bool
+    let code : Int
     let message : String
+    let result : SignUpResult
+}
+
+struct SignUpResult: Codable {
+    
+}
+
+struct UserSignUpRequest {
+    let email : String
+    let password : String
+    let nickname : String
+    let isOptionAgr : Bool
+    let language : String
 }
