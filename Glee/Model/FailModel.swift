5d6402aa8f918d8807f5285b3ea03ab4453bbbb5
gomhyeok <gomhyeok@gmail.com>
2024-05-23 02:11:09 +0900

    Feat : API 연동

---
+++ Glee/Model/FailModel.swift
@@ -0,0 +1,8 @@
+//
+//  FailModel.swift
+//  Glee
+//
+//  Created by 최재혁 on 5/15/24.
+//
+
+import Foundation
---
+++ Glee/Views/FileUpload.swift
@@ -0,0 +1,18 @@
+//
+//  FileUpload.swift
+//  Glee
+//
+//  Created by 최재혁 on 5/14/24.
+//
+
+import SwiftUI
+
+struct FileUpload: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileUpload()
+}
